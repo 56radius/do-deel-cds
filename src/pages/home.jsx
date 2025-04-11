1865faa6b1fc3ffb4cab52cc0e925c35dcb1388f
Merit Mohammed <cussmeritmohammed@gmail.com>
2025-04-11 15:28:38 +0100

    creating the about section

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -77,43 +77,12 @@
                   />
                 </svg>
               </a>
-
-              {/* Highlights */}
-              <div className="grid grid-cols-1 px-20 mt-12 text-left gap-x-12 gap-y-8 sm:grid-cols-3 sm:px-0">
-                <div className="flex items-center">
-                  <svg width="31" height="25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M25.1667 14.187H20.3333C17.6637 14.187 15.5 16.3507 15.5 19.0203V19.8258C15.5 19.8258 18.0174 20.6314 22.75 20.6314C27.4826 20.6314 30 19.8258 30 19.8258V19.0203C30 16.3507 27.8363 14.187 25.1667 14.187Z" stroke="#28CC9D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path d="M18.7227 6.9369C18.7227 4.71276 20.5263 2.90912 22.7504 2.90912C24.9746 2.90912 26.7782 4.71276 26.7782 6.9369C26.7782 9.16104 24.9746 11.7702 22.7504 11.7702C20.5263 11.7702 18.7227 9.16104 18.7227 6.9369Z" stroke="#28CC9D" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path d="M13.2231 15.8512H7.11157C3.73595 15.8512 1 18.5871 1 21.9628V22.9814C1 22.9814 4.18311 24 10.1674 24C16.1516 24 19.3347 22.9814 19.3347 22.9814V21.9628C19.3347 18.5871 16.5988 15.8512 13.2231 15.8512Z" fill="#0B1715" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path d="M5.07422 6.68386C5.07422 3.87152 7.35485 1.59088 10.1672 1.59088C12.9795 1.59088 15.2602 3.87152 15.2602 6.68386C15.2602 9.4962 12.9795 12.7954 10.1672 12.7954C7.35485 12.7954 5.07422 9.4962 5.07422 6.68386Z" fill="#0B1715" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
-                  <p className="ml-3 text-sm">Over 1000 corpers </p>
-                </div>
-
-                <div className="flex items-center">
-                  <svg width="23" height="23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M19.8335 21.9166H3.16683C2.6143 21.9166 2.08439 21.6972 1.69369 21.3065C1.30299 20.9158 1.0835 20.3858 1.0835 19.8333V3.16665C1.0835 2.61411 1.30299 2.08421 1.69369 1.69351C2.08439 1.30281 2.6143 1.08331 3.16683 1.08331H19.8335C20.386 1.08331 20.9159 1.30281 21.3066 1.69351C21.6973 2.08421 21.9168 2.61411 21.9168 3.16665V19.8333C21.9168 20.3858 21.6973 20.9158 21.3066 21.3065C20.9159 21.6972 20.386 21.9166 19.8335 21.9166Z"
-                      stroke="white"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  <p className="ml-3 text-sm">Innovation and impact</p>
-                </div>
-
-                <div className="flex items-center">
-                  <svg width="24" height="24" fill="none">
-                    <circle cx="12" cy="12" r="11" stroke="white" strokeWidth="1.5" />
-                    <path d="M8 12.5L11 15.5L16 10" stroke="#28CC9D" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  </svg>
-                  <p className="ml-3 text-sm"> Projects </p>
-                </div>
-              </div>
             </div>
           </div>
         </section>
+
+        {/* About section */}
+
       </div>
     );
   };
