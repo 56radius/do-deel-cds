5f0b5107c5b49b1c1acc18610d074547a9203069
Merit Mohammed <cussmeritmohammed@gmail.com>
2025-04-12 19:48:45 +0100

    separated the about us into a component folder for reusable code and code structuring

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+
+// Importing icons
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid';
+
+// Importing images
+import corper from "../assets/images/corpers.jpg";
+
+// Features data
+const features = [
+  {
+    name: 'Time for empowerment',
+    description: 'Empowering Communities through Digital Education and Inclusive Opportunities',
+    icon: ServerIcon,
+  },
+  {
+    name: 'Skill for growth',
+    description: 'Digital Onboarders empowers YNEETs (youth not in education, employment, or training) with in-demand skills needed to access jobs or for starting businesses',
+    icon: CloudArrowUpIcon, // Providing a fallback icon
+  },
+]
+
+const AboutUs = () => {
+  return (
+    <div>
+      {/* About Section */}
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="lg:pt-4 lg:pr-8">
+          <div className="lg:max-w-lg">
+            <p className="mt-2 text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">
+              DO-DEEL OGUN STATE
+            </p>
+            <p className="mt-6 text-lg/8 text-gray-600">
+              When it comes to tech for youths, do-deel is what you are looking for.
+              We are dedicated to transforming lives by providing essential digital skills,
+              supporting local businesses, and fostering sustainable community development.
+            </p>
+            <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+              {features.map((feature, index) => (
+                <div key={index} className="flex items-start gap-6">
+                  {feature.icon ? (
+                    <feature.icon className="h-10 w-10 text-indigo-600" />
+                  ) : (
+                    <img width={80} height={80} src={timer} alt={feature.name} className="flex-shrink-0" />
+                  )}
+                  <div>
+                    <dt className="font-semibold text-gray-900 text-xl">{feature.name}</dt>
+                    <dd className="mt-2">{feature.description}</dd>
+                  </div>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
+        <img
+          alt="Product screenshot"
+          src={corper}
+          width={2432}
+          height={1442}
+          className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
